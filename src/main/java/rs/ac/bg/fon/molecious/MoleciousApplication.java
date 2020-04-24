61d39379202a2027edea72691178bc0f44a353f2
Milan <milan_susa@hotmail.com>
2020-04-24 20:46:59 +0200

    Initial commit

---
+++ src/main/java/rs/ac/bg/fon/molecious/MoleciousApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.molecious;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoleciousApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoleciousApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rs/ac/bg/fon/molecious/MoleciousApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.molecious;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoleciousApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
