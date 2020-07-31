aa50e8d3b2d95b3f3a29e8dfb7627020289f71cf
Milan <milan_susa@hotmail.com>
2020-07-31 22:12:36 +0200

    Add tests for context controller creation

--- src/test/java/rs/ac/bg/fon/molecious/MoleciousApplicationTests.java
+++ src/test/java/rs/ac/bg/fon/molecious/MoleciousApplicationTests.java
@@ -1,13 +1,32 @@
 package rs.ac.bg.fon.molecious;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import rs.ac.bg.fon.molecious.controller.InferenceController;
+import rs.ac.bg.fon.molecious.controller.UserController;
 
 @SpringBootTest
-class MoleciousApplicationTests {
+public class MoleciousApplicationTests {
+
+    @Autowired
+    private InferenceController inferenceController;
+    @Autowired
+    private UserController userController;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+    }
+
+    @Test
+    public void contextWhenLoadedShouldCreateInferenceController() {
+        Assertions.assertThat(inferenceController).isNotNull();
+    }
+
+    @Test
+    public void contextWhenLoadedShouldCreateUserController() {
+        Assertions.assertThat(userController).isNotNull();
     }
 
 }
